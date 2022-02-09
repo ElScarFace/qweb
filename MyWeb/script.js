9ba885774eac79e3cd4aa165ae879a70b40fb750
renzo delgado <64338288+Ilomilo18@users.noreply.github.com>
2022-02-09 18:22:41 -0500

    Add files via upload

---
+++ MyWeb/script.js
@@ -0,0 +1,18 @@
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference
+}
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
+
+/*contacto*/
+// ↓↓↓↓↓↓↓____ Esto solo aplica clases de estilos, no afecta el funcionamiento ____ ↓↓↓↓↓↓↓
+
+
+
+
+
